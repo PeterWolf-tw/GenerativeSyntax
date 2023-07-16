2fe9755b1052be13d02c282f7bb4a182b4c419f6
PeterWolf <peterwolf.wang@gmail.com>
2023-07-16 15:00:27 +0800

    調整 regex 比對順序，以便從遠至近檢驗 c-command 關係

--- Tree/CCMD.py
+++ Tree/CCMD.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
+import logging
+logFORMAT = "%(funcName)s>>%(message)s"
+logging.basicConfig(level=logging.INFO, format=logFORMAT)
 import re
 
 from ArticutAPI import Articut
 articut = Articut()
 
 #描述各種節點的 C-Command 關係 (以下先列出幾種)
-CCMDPatDICT = {"ACTION_verb":re.compile("<ACTION_verb>[^<]+</ACTION_verb>(.+<FUNC_inner>的</FUNC_inner>(<MODIFIER>[^<]+</MODIFIER>)?)?(<ENTITY_noun>[^<]+?</ENTITY_noun>|<ENTITY_pronoun>[^<]+?</ENTITY_pronoun>|<ENTITY_person>[^<]+?</ENTITY_person>)"),
-               "VerbP":re.compile("<VerbP>[^<]+</VerbP>(.+<FUNC_inner>的</FUNC_inner>(<MODIFIER>[^<]+</MODIFIER>)?)?(<ENTITY_noun>[^<]+?</ENTITY_noun>|<ENTITY_pronoun>[^<]+?</ENTITY_pronoun>|<ENTITY_person>[^<]+?</ENTITY_person>)"),
-               "FUNC_negation":re.compile("<FUNC_negation>[^<]+</FUNC_negation>(.+)?<ACTION_verb>[^<]+?</ACTION_verb>(.+<FUNC_inner>的</FUNC_inner>(<MODIFIER>[^<]+</MODIFIER>)?)?(<ENTITY_noun>[^<]+?</ENTITY_noun>|<ENTITY_pronoun>[^<]+?</ENTITY_pronoun>|<ENTITY_person>[^<]+?</ENTITY_person>)"),
-               "QUANTIFIER":re.compile("<QUANTIFIER>[^<]+</QUANTIFIER>(.+<FUNC_inner>的</FUNC_inner>(<MODIFIER>[^<]+</MODIFIER>)?)?(<ENTITY_noun>[^<]+?</ENTITY_noun>|<ENTITY_pronoun>[^<]+?</ENTITY_pronoun>|<ENTITY_person>[^<]+?</ENTITY_person>)"),
-               "ENTITY_noun":re.compile("(.+<FUNC_inner>的</FUNC_inner>)?<ENTITY_noun>[^<]+</ENTITY_noun>(<VerbP>[^<]+?</VerbP>|<ACTION_verb>[^<]+?</ACTION_verb>|<FUNC_inner>的</FUNC_inner>)((?<!的<FUNC_inner>)<ENTITY_noun>[^<]+?</ENTITY_noun>|<ENTITY_pronoun>[^<]+?</ENTITY_pronoun>|<ENTITY_person>[^<]+?</ENTITY_person>)((<VerbP>[^<]+?</VerbP>|<ACTION_verb>[^<]+?</ACTION_verb>|<FUNC_inner>的</FUNC_inner>)(<ENTITY_noun>[^<]+?</ENTITY_noun>|<ENTITY_pronoun>[^<]+?</ENTITY_pronoun>|<ENTITY_person>[^<]+?</ENTITY_person>))?"),
-               "ENTITY_pronoun":re.compile("(.+<FUNC_inner>的</FUNC_inner>)?<ENTITY_pronoun>[^<]+</ENTITY_pronoun>(<VerbP>[^<]+?</VerbP>|<ACTION_verb>[^<]+?</ACTION_verb>)((?<!的<FUNC_inner>)<ENTITY_noun>[^<]+?</ENTITY_noun>|<ENTITY_pronoun>[^<]+?</ENTITY_pronoun>|<ENTITY_person>[^<]+?</ENTITY_person>)((<VerbP>[^<]+?</VerbP>|<ACTION_verb>[^<]+?</ACTION_verb>|<FUNC_inner>的</FUNC_inner>)(<ENTITY_noun>[^<]+?</ENTITY_noun>|<ENTITY_pronoun>[^<]+?</ENTITY_pronoun>|<ENTITY_person>[^<]+?</ENTITY_person>))?"),
-               "ENTITY_person":re.compile("(.+<FUNC_inner>的</FUNC_inner>)?<ENTITY_person>[^<]+</ENTITY_person>(<VerbP>[^<]+?</VerbP>|<ACTION_verb>[^<]+?</ACTION_verb>)((?<!的<FUNC_inner>)<ENTITY_noun>[^<]+?</ENTITY_noun>|<ENTITY_pronoun>[^<]+?</ENTITY_pronoun>|<ENTITY_person>[^<]+?</ENTITY_person>)((<VerbP>[^<]+?</VerbP>|<ACTION_verb>[^<]+?</ACTION_verb>|<FUNC_inner>的</FUNC_inner>)(<ENTITY_noun>[^<]+?</ENTITY_noun>|<ENTITY_pronoun>[^<]+?</ENTITY_pronoun>|<ENTITY_person>[^<]+?</ENTITY_person>))?")
+CCMDPatDICT = {"ACTION_verb":[re.compile("<ACTION_verb>[^<]+</ACTION_verb>(.+<FUNC_inner>的</FUNC_inner>(<MODIFIER>[^<]+</MODIFIER>)?)?(<ENTITY_noun>[^<]+?</ENTITY_noun>|<ENTITY_pronoun>[^<]+?</ENTITY_pronoun>|<ENTITY_person>[^<]+?</ENTITY_person>)")],
+               "VerbP":[re.compile("<VerbP>[^<]+</VerbP>(.+<FUNC_inner>的</FUNC_inner>(<MODIFIER>[^<]+</MODIFIER>)?)?(<ENTITY_noun>[^<]+?</ENTITY_noun>|<ENTITY_pronoun>[^<]+?</ENTITY_pronoun>|<ENTITY_person>[^<]+?</ENTITY_person>)")],
+               "FUNC_negation":[re.compile("<FUNC_negation>[^<]+</FUNC_negation>(.+)?<ACTION_verb>[^<]+?</ACTION_verb>(.+<FUNC_inner>的</FUNC_inner>(<MODIFIER>[^<]+</MODIFIER>)?)?(<ENTITY_noun>[^<]+?</ENTITY_noun>|<ENTITY_pronoun>[^<]+?</ENTITY_pronoun>|<ENTITY_person>[^<]+?</ENTITY_person>)")],
+               "QUANTIFIER":[re.compile("<QUANTIFIER>[^<]+</QUANTIFIER>(.+<FUNC_inner>的</FUNC_inner>(<MODIFIER>[^<]+</MODIFIER>)?)?(<ENTITY_noun>[^<]+?</ENTITY_noun>|<ENTITY_pronoun>[^<]+?</ENTITY_pronoun>|<ENTITY_person>[^<]+?</ENTITY_person>)")],
+               "ENTITY_noun":[re.compile("(.+<FUNC_inner>的</FUNC_inner>)?<ENTITY_noun>[^<]+</ENTITY_noun>(<VerbP>[^<]+?</VerbP>|<ACTION_verb>[^<]+?</ACTION_verb>)((?<!的<FUNC_inner>)<ENTITY_noun>[^<]+?</ENTITY_noun>|<ENTITY_pronoun>[^<]+?</ENTITY_pronoun>|<ENTITY_person>[^<]+?</ENTITY_person>)((<VerbP>[^<]+?</VerbP>|<ACTION_verb>[^<]+?</ACTION_verb>|<FUNC_inner>的</FUNC_inner>)(<ENTITY_noun>[^<]+?</ENTITY_noun>|<ENTITY_pronoun>[^<]+?</ENTITY_pronoun>|<ENTITY_person>[^<]+?</ENTITY_person>))?"),
+                              re.compile("<ENTITY_noun>[^<]+</ENTITY_noun><FUNC_inner>的</FUNC_inner>(<ENTITY_noun>[^<]+?</ENTITY_noun>|<ENTITY_pronoun>[^<]+?</ENTITY_pronoun>|<ENTITY_person>[^<]+?</ENTITY_person>)"),
+                              ],
+               "ENTITY_pronoun":[re.compile("(.+<FUNC_inner>的</FUNC_inner>)?<ENTITY_pronoun>[^<]+</ENTITY_pronoun>(<VerbP>[^<]+?</VerbP>|<ACTION_verb>[^<]+?</ACTION_verb>)((?<!的<FUNC_inner>)<ENTITY_noun>[^<]+?</ENTITY_noun>|<ENTITY_pronoun>[^<]+?</ENTITY_pronoun>|<ENTITY_person>[^<]+?</ENTITY_person>)((<VerbP>[^<]+?</VerbP>|<ACTION_verb>[^<]+?</ACTION_verb>|<FUNC_inner>的</FUNC_inner>)(<ENTITY_noun>[^<]+?</ENTITY_noun>|<ENTITY_pronoun>[^<]+?</ENTITY_pronoun>|<ENTITY_person>[^<]+?</ENTITY_person>))?")],
+               "ENTITY_person":[re.compile("(.+<FUNC_inner>的</FUNC_inner>)?<ENTITY_person>[^<]+</ENTITY_person>(<VerbP>[^<]+?</VerbP>|<ACTION_verb>[^<]+?</ACTION_verb>)((?<!的<FUNC_inner>)<ENTITY_noun>[^<]+?</ENTITY_noun>|<ENTITY_pronoun>[^<]+?</ENTITY_pronoun>|<ENTITY_person>[^<]+?</ENTITY_person>)((<VerbP>[^<]+?</VerbP>|<ACTION_verb>[^<]+?</ACTION_verb>|<FUNC_inner>的</FUNC_inner>)(<ENTITY_noun>[^<]+?</ENTITY_noun>|<ENTITY_pronoun>[^<]+?</ENTITY_pronoun>|<ENTITY_person>[^<]+?</ENTITY_person>))?")]
               }
 
 def CCMDchecker(inputSTR, argX, argY):
@@ -41,12 +46,14 @@ def CCMDchecker(inputSTR, argX, argY):
         argXPOS = argXPOSLIST[-1]
 
     if argXPOS in CCMDPatDICT.keys():
-        checkingTagLIST =[p.group(0) for p in  CCMDPatDICT[argXPOS].finditer(inputTagSTR)]
-        for c in checkingTagLIST:
-            if c.startswith(argXTagSTR) and argY in c:
-                return True
-            else:
-                pass
+        for xPat in range(0, len(CCMDPatDICT[argXPOS])):
+            checkingTagLIST =[p.group(0) for p in  CCMDPatDICT[argXPOS][xPat].finditer(inputTagSTR)]
+            for c in checkingTagLIST:
+                logging.info(c)
+                if c.startswith(argXTagSTR) and argY in c:
+                    return True
+                else:
+                    pass
     else:
         resultBOOL = "Unknown"
 
@@ -56,11 +63,11 @@ def CCMDchecker(inputSTR, argX, argY):
 if __name__ == "__main__":
     inputDICT = {"inputSTR":"任何人沒吃晚餐", "argX":"任何", "argY":"人"}               #True
     inputDICT = {"inputSTR":"老張批評了自己", "argX":"老張", "argY":"自己"}             #True
-    inputDICT = {"inputSTR":"老張的爸爸批評了自己", "argX":"老張", "argY":"自己"}        #False
-    #inputDICT = {"inputSTR":"老張的爸爸批評了自己", "argX":"老張", "argY":"爸爸"}        #True
-    #inputDICT = {"inputSTR":"老張的爸爸批評了自己", "argX":"老張的爸爸", "argY":"自己"}   #True
-    #inputDICT = {"inputSTR":"老張知道約翰喜歡自己", "argX":"老張", "argY":"自己"}        #True
-    #inputDICT = {"inputSTR":"老張知道約翰喜歡自己", "argX":"約翰", "argY":"自己"}        #True
+    inputDICT = {"inputSTR":"老張的爸爸批評了自己", "argX":"老張", "argY":"自己"}       #False
+    inputDICT = {"inputSTR":"老張的爸爸批評了自己", "argX":"老張", "argY":"爸爸"}       #True
+    inputDICT = {"inputSTR":"老張的爸爸批評了自己", "argX":"老張的爸爸", "argY":"自己"} #True
+    inputDICT = {"inputSTR":"老張知道約翰喜歡自己", "argX":"老張", "argY":"自己"}       #True
+    inputDICT = {"inputSTR":"老張知道約翰喜歡自己", "argX":"約翰", "argY":"自己"}       #True
 
 
     result = CCMDchecker(inputSTR=inputDICT["inputSTR"], argX=inputDICT["argX"], argY=inputDICT["argY"])
